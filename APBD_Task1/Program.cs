8c6734a5e3ac8ab8dd34ba50f934155df7df988e
Anton Churchyn <s32801@pjwstk.edu.pl>
2026-03-10 17:02:34 +0100

    meaningfull commit 1

--- APBD_Task1/Program.cs
+++ APBD_Task1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Task1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two numbers to calculate the sum");
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+            Console.WriteLine(number1 + number2);
+        }
+    }
+}
