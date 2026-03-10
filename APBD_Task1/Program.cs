a6e65781b7f4f2bd1d3abe65cc30fe2fb978461f
Anton Churchyn <s32801@pjwstk.edu.pl>
2026-03-10 17:07:08 +0100

    concatenation 2

--- APBD_Task1/Program.cs
+++ APBD_Task1/Program.cs
@@ -8,6 +8,11 @@
             int number1 = int.Parse(Console.ReadLine());
             int number2 = int.Parse(Console.ReadLine());
             Console.WriteLine(number1 + number2);
+
+            Console.WriteLine("Concatenate two strings");
+            var str1 = Console.ReadLine();
+            var str2 = Console.ReadLine();
+            Console.WriteLine(str1 + str2);
         }
     }
 }
