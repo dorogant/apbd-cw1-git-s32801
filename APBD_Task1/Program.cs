2d157579555ba69e48f053b4c50fe15a9c4ee8db
Anton Churchyn <s32801@pjwstk.edu.pl>
2026-03-11 14:20:38 +0100

    clean-up, improving algorithms

--- APBD_Task1/Program.cs
+++ APBD_Task1/Program.cs
@@ -4,16 +4,6 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Enter two numbers to calculate the sum");
-            int number1 = int.Parse(Console.ReadLine());
-            int number2 = int.Parse(Console.ReadLine());
-            Console.WriteLine(number1 + number2);
-
-            Console.WriteLine("Concatenate two strings");
-            var str1 = Console.ReadLine();
-            var str2 = Console.ReadLine();
-            Console.WriteLine(str1 + str2);
-            
             Console.WriteLine("Enter an array of numbers");
             int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
             Console.WriteLine("avg is: " + calculateAverage(arr));
@@ -22,7 +12,7 @@
         }
 
         public static double calculateAverage(int[] arr)
-        { /// trying again
+        {
             double sum = 0;
             foreach (var v in arr)
             {
@@ -32,7 +22,7 @@
         }
         public static int calculateMax(int[] arr)
         {
-            int max = 0;
+            int max = arr[0];
             foreach (var v in arr)
             {
                 if (max < v) max = v;
@@ -41,7 +31,7 @@
         }
         public static int calculateMin(int[] arr)
         {
-            int min = 0;
+            int min = arr[0];
             foreach (var v in arr)
             {
                 if (min > v) min = v;
